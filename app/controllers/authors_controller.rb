001369e5505801ca9d5ae5f128e5d7e6c283b0c4
github username <kelly.hesterly@gmail.com>
2021-10-22 10:35:07 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,9 +9,10 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
+    
     if @author.valid?
-      @author = Author.create(author_params)
-      redirect_to author_path(@author)
+        @author.save
+       redirect_to author_path(@author)
     else
       render :new
     end
